cf7825e60f3049743756a788c949261d58327acb
xjchong <xavier.chong.j@gmail.com>
2019-06-07 09:23:49 -0400

    chore: setup project
    - include cocoapods (alamofire, promise)
    - basic splash screen

---
+++ Innventory/Pods/Target Support Files/Pods-Innventory/Pods-Innventory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Innventory : NSObject
+@end
+@implementation PodsDummy_Pods_Innventory
+@end
---
+++ Innventory/Pods/Target Support Files/Pods-InnventoryTests/Pods-InnventoryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InnventoryTests : NSObject
+@end
+@implementation PodsDummy_Pods_InnventoryTests
+@end
